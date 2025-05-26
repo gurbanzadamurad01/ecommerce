a7405b4e00b2066231566e0c2b813840aa10d69a
Murad Gurbanzada <gurbanzadamurad@gmail.com>
2025-05-27 03:29:30 +0400

    fix auth

--- src/app/(app)/(auth)/sign-in/page.tsx
+++ src/app/(app)/(auth)/sign-in/page.tsx
@@ -1,8 +1,17 @@
+import { caller } from "@/trpc/server";
+
+import { redirect } from "next/navigation";
+
 import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
 
 export const dynamic = "force-dynamic";
 
-const Page = () => {
+const Page = async() => {
+  const session = await caller.auth.session();
+
+  if (session.user) {
+    redirect("/");
+  }
   return <SignInView />;
 };
 
